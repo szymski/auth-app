00e3e94cfb856b9db51eae9c7d819beee51be024
szymski <szymskipl@gmail.com>
2020-08-12 20:50:48 +0200

    Add auth guard to dashboard controller

--- backend/src/dashboard/dashboard.controller.ts
+++ backend/src/dashboard/dashboard.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { SavedExchangeRates } from './entities/exchange-rates.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('dashboard')
+@UseGuards(AuthGuard())
 export class DashboardController {
 
   constructor(private dashboardService: DashboardService) {
