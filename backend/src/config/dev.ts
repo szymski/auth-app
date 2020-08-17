8189dc2ec12930788328b5e191b7df5ed8ee9288
Szymon Jankowski <szymskipl@gmail.com>
2020-08-17 11:19:42 +0200

    Update connection string in docker-compose file

--- backend/src/config/dev.ts
+++ backend/src/config/dev.ts
@@ -5,7 +5,7 @@ export default {
     port: process.env.PORT ?? 3000,
   },
   db: {
-    connectionString: "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+    connectionString: process.env.CONNECTION_STRING ?? "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false",
   },
   jwt: {
     secret: "DEV"
